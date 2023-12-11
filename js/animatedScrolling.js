70304c54c811365f31e9825eb7b406f562a2811d
Garrett <gad50@txstate.edu>
2023-12-11 16:28:28 -0600

    made changes to index.html

--- js/animatedScrolling.js
+++ js/animatedScrolling.js
@@ -1,16 +1,38 @@
-let scrollAmount = 0;
+const gallery = document.querySelector('.scrollable-gallery');
+const images = gallery.querySelectorAll('img');
+const navigation = document.querySelector('.gallery-navigation');
+let currentIndex = 0;
+
+// Create navigation circles
+images.forEach((img, index) => {
+    const circle = document.createElement('span');
+    circle.addEventListener('click', () => moveToIndex(index));
+    navigation.appendChild(circle);
+});
+
+function updateNavigation() {
+    navigation.querySelectorAll('span').forEach((circle, index) => {
+        circle.style.backgroundColor = index === currentIndex ? '#000' : '#bbb';
+    });
+}
+
+function moveToIndex(index) {
+    currentIndex = index;
+    const position = -200 * index; // 200 is the width of the image
+    gallery.style.transform = `translateX(${position}px)`;
+    updateNavigation();
+}
 
 document.getElementById('scrollRight').addEventListener('click', () => {
-    const gallery = document.querySelector('.scrollable-gallery');
-    if (scrollAmount < gallery.scrollWidth - gallery.clientWidth) {
-        scrollAmount += 200; // Width of the image
+    if (currentIndex < images.length - 1) {
+        moveToIndex(currentIndex + 1);
     }
-    gallery.style.transform = `translateX(-${scrollAmount}px)`;
 });
 
 document.getElementById('scrollLeft').addEventListener('click', () => {
-    if (scrollAmount > 0) {
-        scrollAmount -= 200;
+    if (currentIndex > 0) {
+        moveToIndex(currentIndex - 1);
     }
-    document.querySelector('.scrollable-gallery').style.transform = `translateX(-${scrollAmount}px)`;
 });
+
+updateNavigation(); // Initial update for navigation circles
