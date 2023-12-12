a793f9e2c0e0caeac8e3933ab4d876e71c29b3c0
Garrett <gad50@txstate.edu>
2023-12-12 13:29:42 -0600

    added OLC video

--- js/slideshow.js
+++ js/slideshow.js
@@ -18,11 +18,17 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.opacity = "0";  
   }
+  setTimeout(function() {
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.opacity = "1";  
+  }, 500); // Duration of fade effect in milliseconds
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
