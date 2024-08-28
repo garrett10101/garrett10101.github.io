c085995714c02d40f3af1a33581a261b6d5e6e09
Garrett DiPalma <garrettdipalma2@gmail.com>
2024-08-28 17:01:53 +0000

    edit app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,12 +5,13 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import About from './components/About';
 import Contact from './components/Contact';
+import Skills from './components/Skills';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <Router basename="/garrett10101.github.io">
+    <Router>
       <div className="App" style={{ backgroundColor: 'black' }}>
         <Navbar />
         <Routes>
@@ -18,6 +19,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/skills" element={<Skills />} />
         </Routes>
         <Footer />
       </div>
