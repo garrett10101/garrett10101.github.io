fc97a8a888371779951c26cc9890a8d633b073c2
Garrett DiPalma <garrettdipalma2@gmail.com>
2024-09-27 15:07:33 +0000

    fix

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaLinkedin, FaGithub, FaDownload } from 'react-icons/fa';
 import './Footer.css';
-import resumeFile from 'assets/Garrett_Resume - Edited Resume.pdf';
+import resumeFile from '/assets/Garrett_Resume - Edited Resume.pdf';
 const Footer = () => {
   return (
     <footer>
