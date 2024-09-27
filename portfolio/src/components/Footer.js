60c5c09c5f57de171c29d5a1b42b676e05f32128
Garrett DiPalma <garrettdipalma2@gmail.com>
2024-09-27 15:04:52 +0000

    tried to fix resumeFile

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { FaLinkedin, FaGithub, FaDownload } from 'react-icons/fa';
 import './Footer.css';
-import resumeFile from './public/assets/Garrett_DiPalma - Edited Resume.pdf';
-
+import resumeFile from 'assets/Garrett_Resume - Edited Resume.pdf';
 const Footer = () => {
   return (
     <footer>
