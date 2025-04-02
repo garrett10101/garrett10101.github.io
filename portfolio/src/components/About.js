f91d17a19c7b38683637f2fd2c04da1d44e52f67
Garrett DiPalma <garrettdipalma2@gmail.com>
2025-04-02 13:36:54 -0500

    fix: update employment dates in About section

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -13,8 +13,8 @@ const About = () => {
       </p>
       <h3>Relevant Experience/Projects</h3>
       <ul>
-        <li><strong>IT Technician - QASystems, AISD Contractor</strong> (<i>03/2024 - Present</i>) - Provided Tier 1 hardware support focusing on Chromebook repair. Conducted QC checks, inventory sorting, and other assigned projects. Used inventory management software for record-keeping.</li>
         <li><strong>IT Assistant - Meadows Center, Texas State University</strong> (<i>02/2023 - Present</i>) - Installed, set up, and configured WeeWX on an Intel NUC and a Davis Vantage Pro 2 station. Managed Apache and FTP for device backup, improving data accuracy and reporting efficiency.</li>
+        <li><strong>IT Technician - QASystems, AISD Contractor</strong> (<i>03/2024 - 01/2025</i>) - Provided Tier 1 hardware support focusing on Chromebook repair. Conducted QC checks, inventory sorting, and other assigned projects. Used inventory management software for record-keeping.</li>
         <li><strong>Computer Technician - GTS Technologies</strong> (<i>06/2023 - 09/2023</i>) - Conducted Quality Assurance on Chromebooks, iPads, and other devices. Led the update of Inventory Management Software and Database Management System, ensuring smooth network operations.</li>
       </ul>
       <section id="skills">
